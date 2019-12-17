6c3eb841e0f419a26e51cbd707c5801dcac3cfa1
davidbarat <dav.barat@gmail.com>
2019-12-17 23:20:37 +0100

    creation boucle pour lecture reponse API OFF

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # OpenFoodFacts
+
+
+database openfoodfacts
+une table produits_recupérés (id, aliment, catégorie, description, magasin, lien)
+          produits_selectionnés (choix aliments, substitus sélectionnés)
+
+clé etrangère aliment
+
+
+class database
+	def create
+	def select
+	def insert
+	def truncate
+
+
+create menu
+(1) choisir une catégorie
+	(1.2) choisir un produit parmi quelques produits malsains dans cette catégorie 
+	(1.3) choisir un substitut parmi le ou les substituts proposés
+(2) choisir d'enregistrer ou pas le substitut en favoris
+
+
+tags json
+code
+url
+product_name
+ingredient_text
+stores
+categories
+nutrition_grade_fr
+additives_n
+
--- main.py
+++ main.py
@@ -7,44 +7,45 @@ import random
 # api-endpoint 
 url = "https://fr.openfoodfacts.org/cgi/search.pl"
 payload = {
-    "tag_0": "pizza",
+    "tag_0": "snack",
     "tag_contains_0": "contains",
     "tagtype_0": "categories",
     "sort_by": "unique_scans_n",
-    "page_size": 1000,
     "page": 1,
+    "page_size": 20,
     "action": "process",
     "json": 1
 }
 
 # data = "https://fr.openfoodfacts.org/cgi/search.pl?action=process&json=1&search_terms2=pates-a-tartiner"
 # sending get request and saving the response as response object 
-r = requests.get(
-    url = url,
-    params = payload,
-    headers = {'UserAgent': 'Project OpenFood - MacOS - Version 10.13.6'}
-    )
+# r = requests.get(
+#     url = url,
+#     params = payload,
+#     headers = {'UserAgent': 'Project OpenFood - MacOS - Version 10.13.6'}
+#     )
 
 # extracting data in json format 
-data = r.json() 
-
-# print(data)
-for i in range(10):
-    print(data['products'][i]['product_name'])
-    print(data['products'][i]['categories'])
-    print(data['products'][i]['nutriscore_grade'])
-    # print(data['products'][i]['stores'])
-    print(data['products'][i]['url'])
-    # print(data['products'][i]['additives_n'])
-    print('----')
-
-# extracting latitude, longitude and formatted address 
-# of the first matching location 
-# latitude = data['results'][0]['geometry']['location']['lat'] 
-# longitude = data['results'][0]['geometry']['location']['lng'] 
-# formatted_address = data['results'][0]['formatted_address']
+# data = r.json() 
 
 
-
-answer = input('Entrez votre nombre: ')
-print(answer)
+for i in range(20):
+    r = requests.get(
+        url = url,
+        params = payload,
+        headers = {'UserAgent': 'Project OpenFood - MacOS - Version 10.13.6'}
+        )
+    data = r.json()
+    # print(data)
+    for j in range(1, 20, 1):
+        print(data['products'][j]['product_name'])
+        print(data['products'][j]['categories'])
+        # print(data['products'][j]['nutriscore_grade'])
+        print(data['products'][j]['stores_tags'])
+        print(data['products'][j]['url'])
+        print(data['products'][j]['additives_n'])
+        print('----')
+        # print(j)
+    payload['page'] = i
+# answer = input('Entrez votre nombre: ')
+# print(answer)
